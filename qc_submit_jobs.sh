61b20d23decfd3a6370b6fe2e0fc9da46164f9c0
Ananth-Prakash <Ananth.Surappa-Narayanappa@jic.ac.uk>
2019-05-20 15:40:01 +0100

    QC for Nipponbare and 9311

--- qc_submit_jobs.sh
+++ qc_submit_jobs.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -e
 
-#SBATCH --job-name=maticM
-#SBATCH -o Batch_maticM.out
-#SBATCH -e Batch_maticM.err
+## Script to assess quality of the reads [QC]
+
+#SBATCH --job-name=QC
+#SBATCH -o Batch_QC.out
+#SBATCH -e Batch_QC.err
 #SBATCH --mem 2000
 #SBATCH -p nbi-short   # Partition (queue equivalent)
 
-export SBATCH_PARTITION=nbi-long 
 
 source fastqc-0.11.3
 
+# Nipponbare
 sbatch --wrap 'fastqc ./../Trimmed_files/NmNAI_B1_R1.fastq.gz -o ./'
 sbatch --wrap 'fastqc ./../Trimmed_files/NmNAI_B1_R2.fastq.gz -o ./'
 sbatch --wrap 'fastqc ./../Trimmed_files/NmNAI_B2_R1.fastq.gz -o ./'
@@ -19,3 +21,15 @@ sbatch --wrap 'fastqc ./../Trimmed_files/NpNAI_B1_R1.fastq.gz -o ./'
 sbatch --wrap 'fastqc ./../Trimmed_files/NpNAI_B1_R2.fastq.gz -o ./'
 sbatch --wrap 'fastqc ./../Trimmed_files/NpNAI_B2_R1.fastq.gz -o ./'
 sbatch --wrap 'fastqc ./../Trimmed_files/NpNAI_B2_R2.fastq.gz -o ./'
+
+
+# 9311
+sbatch --wrap 'fastqc ./../Trimmed_files/9mNAI_Bio1_forwardread_trimmed.fastq -o ./'
+sbatch --wrap 'fastqc ./../Trimmed_files/9mNAI_Bio1_reverseread_trimmed.fastq -o ./'
+sbatch --wrap 'fastqc ./../Trimmed_files/9mNAI_Bio2_forwardread_trimmed.fastq -o ./'
+sbatch --wrap 'fastqc ./../Trimmed_files/9mNAI_Bio2_reverseread_trimmed.fastq -o ./'
+
+sbatch --wrap 'fastqc ./../Trimmed_files/9pNAI_Bio1_forwardread_trimmed.fastq -o ./'
+sbatch --wrap 'fastqc ./../Trimmed_files/9pNAI_Bio1_reverseread_trimmed.fastq -o ./'
+sbatch --wrap 'fastqc ./../Trimmed_files/9pNAI_Bio2_forwardread_trimmed.fastq -o ./'
+sbatch --wrap 'fastqc ./../Trimmed_files/9pNAI_Bio2_reverseread_trimmed.fastq -o ./'
